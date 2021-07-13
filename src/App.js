2ca7069612a888cd9ece42b3cbfbf03f9df20822
barbaroscreative <barbarossozturkk@gmail.com>
2021-07-13 16:00:27 +0300

    Experience companent completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
+import Experience from "./components/Experience";
+
 
 function App() {
   return (
@@ -35,6 +37,7 @@ function App() {
       <Header />
       <AboutMe/>
       <Services/>
+      <Experience/>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
     <div className="container">
 
         <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..." /></a>
@@ -15,8 +15,8 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav  ms-auto">
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#">Home</a>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">about me</a>
@@ -25,7 +25,7 @@ const Navbar = () => {
               <a className="nav-link" href="#">services</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">how work</a>
+              <a className="nav-link" href="#">experience</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">portfolio</a>
